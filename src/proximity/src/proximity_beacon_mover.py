61a64f2ff3a9b0da1f1e810728360e25c6deda97
kopop8 <kopop8@live.nl>
2020-04-07 15:35:15 +0200

    Added commented mover back

--- src/proximity/src/proximity_beacon_mover.py
+++ src/proximity/src/proximity_beacon_mover.py
@@ -28,7 +28,46 @@ class FibonacciAction(object):
             self._result.result = self._feedback.state
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
-        
+
+
+
+# def movebase_client(address):
+#     with open('/home/pieter/beacons.yaml') as f:
+#         # use safe_load instead load
+#         dataMap = yaml.load(f,  Loader=yaml.Loader)
+#         rospy.loginfo(dataMap)
+
+#    # Create an action client called "move_base" with action definition file "MoveBaseAction"
+#     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+ 
+#    # Waits until the action server has started up and started listening for goals.
+#     client.wait_for_server()
+
+#    # Creates a new goal with the MoveBaseGoal constructor
+#     goal = MoveBaseGoal()
+#     goal.target_pose.header.frame_id = "map"
+#     goal.target_pose.header.stamp = rospy.Time.now()
+#    # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
+#     goal.target_pose.pose.position.x = 2.30914855003
+#     goal.target_pose.pose.position.y = -0.090583704412
+#     # goal.target_pose.pose.position.z = 0.5
+#    # No rotation of the mobile base frame w.r.t. map frame
+#     goal.target_pose.pose.orientation.w = 1.0
+
+#    # Sends the goal to the action server.
+#     client.send_goal(goal)
+#    # Waits for the server to finish performing the action.
+#     wait = client.wait_for_result()
+#    # If the result doesn't arrive, assume the Server is not available
+#     if not wait:
+#         rospy.logerr("Action server not available!")
+#         rospy.signal_shutdown("Action server not available!")
+#     else:
+#     # Result of executing the action
+#         return client.get_result()   
+
+
+
 if __name__ == '__main__':
     rospy.init_node('move_to_beacon_action')
     server = FibonacciAction(rospy.get_name())
