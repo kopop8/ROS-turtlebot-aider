85878ff57d7d490b44cb25decbfdf73e8c577df6
kopop8 <kopop8@live.nl>
2020-05-27 14:41:15 +0200

    Made simple action client

--- src/proximity/src/frontier_exploration_client.py
+++ src/proximity/src/frontier_exploration_client.py
@@ -7,27 +7,30 @@ import rospy
 import actionlib
 # Brings in the .action file and messages used by the move base action
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-
-def movebase_client():
+from frontier_exploration.msg import ExploreTaskAction, ExploreTaskActionGoal
+from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PolygonStamped
+def frontier_client():
 
    # Create an action client called "move_base" with action definition file "MoveBaseAction"
-    client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
-    rospy.loginfo("connecting")
+    client = actionlib.SimpleActionClient('explore_server',ExploreTaskAction)
+    rospy.loginfo("Connecting to server...")
    # Waits until the action server has started up and started listening for goals.
     client.wait_for_server()
-    rospy.loginfo("connected")
-
+    rospy.loginfo("Server connected!")
    # Creates a new goal with the MoveBaseGoal constructor
-    goal = MoveBaseGoal()
+    goal = ExploreTaskActionGoal()
+    # maybe send explore boundary
+    goal.explore_boundary.header.seq = 1
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-   # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
-    goal.target_pose.pose.position.x = 0.5
-   # No rotation of the mobile base frame w.r.t. map frame
-    goal.target_pose.pose.orientation.w = 1.0
+    goal.explore_center.point.x = 1
+    goal.explore_center.point.y = 0
+    goal.explore_center.point.z = 0
 
    # Sends the goal to the action server.
     client.send_goal(goal)
+    rospy.loginfo("Send goal!")
    # Waits for the server to finish performing the action.
     wait = client.wait_for_result()
    # If the result doesn't arrive, assume the Server is not available
@@ -42,8 +45,9 @@ def movebase_client():
 if __name__ == '__main__':
     try:
        # Initializes a rospy node to let the SimpleActionClient publish and subscribe
-        rospy.init_node('movebase_client_py')
-        result = movebase_client()
+        rospy.init_node('frontier_exploration_client')
+        rospy.loginfo("Node inited")
+        result = frontier_client()
         if result:
             rospy.loginfo("Goal execution done!")
     except rospy.ROSInterruptException:
